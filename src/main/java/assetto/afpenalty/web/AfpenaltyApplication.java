827830ebcc778b8128329125d77c16d50b41892b
Josh Brookes <joshua.brookes.3007@gmail.com>
2022-08-27 23:01:37 +0100

    Generalised package import

--- src/main/java/assetto/afpenalty/web/AfpenaltyApplication.java
+++ src/main/java/assetto/afpenalty/web/AfpenaltyApplication.java
@@ -1,7 +1,6 @@
 package assetto.afpenalty.web;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
 
 @SpringBootApplication
 @EnableAutoConfiguration
