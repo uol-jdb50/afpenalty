0ac0780c34258fd8e3b410c572933933d00246ca
Josh Brookes <joshua.brookes.3007@gmail.com>
2022-08-27 23:20:46 +0100

    Altered stuff, it's late

--- src/main/java/assetto/afpenalty/web/AfpenaltyApplication.java
+++ src/main/java/assetto/afpenalty/web/AfpenaltyApplication.java
@@ -1,8 +1,11 @@
 package assetto.afpenalty.web;
 
-import org.springframework.boot.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("assetto.afpenalty.web")
 public class AfpenaltyApplication {
 
 	public static void main(String[] args) {
